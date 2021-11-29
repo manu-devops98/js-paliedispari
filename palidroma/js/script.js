a955b9963b72bd3cb98a509165b871973725e1f6
manu-devops98 <manuelcappello7@gmail.com>
2021-11-29 14:59:44 +0100

    add fase 1 senza funzione

--- palidroma/js/script.js
+++ palidroma/js/script.js
@@ -3,3 +3,22 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 (prima senza funzione e poi con funzione)
  */
+
+// chiedo all'utente di inserire parola tramite prompt
+
+const userWord = prompt('Inserisci parola');
+
+let userSplit = userWord.split("");
+console.log(userSplit);
+
+let userReverse = userSplit.reverse();
+console.log(userReverse);
+
+let userJoin = userReverse.join('');
+console.log(userJoin);
+
+if (userWord == userJoin) {
+    console.log('Parola palindroma');
+} else {
+    console.log('Non palindroma');
+}
